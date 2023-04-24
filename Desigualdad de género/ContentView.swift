5047bee2a26d6ee3ebd9f86d4fb2ca7c3b7d3c01
GaelMVCAcatlan <317083545@pcpuma.acatlan.unam.mx>
2023-04-24 17:46:13 -0600

    Contador Violentómetro

--- Desigualdad de género/ContentView.swift
+++ Desigualdad de género/ContentView.swift
@@ -9,49 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            var Contador, a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0
+            
+            Text("a) Por favor digita el número que más se relacione con lo que has vivido")
+        if a == 1
+        {
+            Contador += 
         }
-        .padding()
         
-        func preguntar() -> Int
-        {
-            var contadora = 0
-            for x in 0...20
-            {
-                
-            }
-            return contadora
-        }
-        func validarRespuesta(validar: Int)
-        {
-            if validar > 0 && validar < 35
-            {
-                print("Violencia tipo 1")
-            }
-            else
-            {
-                if validar > 35 && validar < 70
-                {
-                    print("¡Cuidado! Violencia tipo 2")
-                }
-                else
-                {
-                    if validar > 70
-                    {
-                        print("¡ESTÁS EN PELIGRO! Violencia tipo 3")
-                    }
-                }
-            }
-        }
+            Text("b) ¿Te han mentido o engañado? \n 1. Nunca   2. Algunas veces   3. Muy seguido")
+            Text("c) ¿Has sido celada? \n 1. Nunca   2. Algunas veces   3. Muy seguido")
+            Text("d) ¿Te han manoseado sin tu consentimiento? \n 1. Nunca   2. Algunas veces   3. Muy seguido")
+            Text("e) ¿Han llegado a golpearte? Inclusive si es de juego \n 1. Nunca   2. Algunas veces   3. Muy seguido")
+            Text("f) ¿Has sido empujada o jaloneada? \n 1. Nunca   2. Algunas veces   3. Muy seguido")
+            Text("g) ¿Te han amenazado de muerte? \n 1. Nunca   2. Algunas veces   3. Muy seguido")
+            Text("h) ¿Han abusado de ti sexualmente? \n 1. Nunca   2. Algunas veces   3. Muy seguido")
+        
+        
+        
+        
+        
+   /*     func preguntar() -> Int
+          {
+              var contadora = 0
+              for _ in 0...20
+              {
+                  
+              }
+              return contadora
+          }
+          func validarRespuesta(validar: Int)
+          {
+              if validar > 0 && validar < 35
+              {
+                  print("Violencia tipo 1")
+              }
+              else
+              {
+                  if validar > 35 && validar < 70
+                  {
+                      print("¡Cuidado! Violencia tipo 2")
+                  }
+                  else
+                  {
+                      if validar > 70
+                      {
+                          print("¡ESTÁS EN PELIGRO! Violencia tipo 3")
+                      }
+                  }
+              }
+          }
+        
+        
 
         var violenciaTotal = preguntar()
         validarRespuesta(validar: <#Int#>)
-
+*/
         
+
     }
 }
 
@@ -60,3 +76,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
