1dd65ec0b2839729f7694d60a158081d1af6ec63
GaelMVCAcatlan <317083545@pcpuma.acatlan.unam.mx>
2023-04-24 12:38:34 -0600

    Initial Commit

---
+++ Desigualdad de género/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Desigualdad de género
+//
+//  Created by CEDAM08 on 24/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Desigualdad de género/Desigualdad_de_ge_neroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desigualdad_de_ge_neroApp.swift
+//  Desigualdad de género
+//
+//  Created by CEDAM08 on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Desigualdad_de_ge_neroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
