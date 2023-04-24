420dc9a10de4c30d864de57eaa97f55bb6d42982
GaelMVCAcatlan <317083545@pcpuma.acatlan.unam.mx>
2023-04-24 16:11:24 -0600

    Función contadora

--- Desigualdad de género/ContentView.swift
+++ Desigualdad de género/ContentView.swift
@@ -16,6 +16,42 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        
+        func preguntar() -> Int
+        {
+            var contadora = 0
+            for x in 0...20
+            {
+                
+            }
+            return contadora
+        }
+        func validarRespuesta(validar: Int)
+        {
+            if validar > 0 && validar < 35
+            {
+                print("Violencia tipo 1")
+            }
+            else
+            {
+                if validar > 35 && validar < 70
+                {
+                    print("¡Cuidado! Violencia tipo 2")
+                }
+                else
+                {
+                    if validar > 70
+                    {
+                        print("¡ESTÁS EN PELIGRO! Violencia tipo 3")
+                    }
+                }
+            }
+        }
+
+        var violenciaTotal = preguntar()
+        validarRespuesta(validar: <#Int#>)
+
+        
     }
 }
 
